bbd665805e6f8eb74d6bcbd0a0b6ee5012cbbe3a
orchidromdev <126561359+theprojectsorchid@users.noreply.github.com>
2023-06-11 21:38:40 +0100

    OrchidOS foss applications
    OrchidOS foss applications

---
+++ prebuilts.mk
@@ -0,0 +1,21 @@
+# Apps
+PRODUCT_PACKAGES += \
+	AdAway \
+	aperture \
+	appman \
+	athena \
+	aves \
+	cal \
+	cleanerplus \
+	connectyou \
+	datamonitor \
+	dualwall \
+	datamonitor \
+	gpt \
+	imagetool \
+	prow \
+	qstiles \
+	recordyou \
+	taptap \
+	Telegram \
+	wifiinfo
